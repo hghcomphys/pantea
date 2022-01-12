0bcbfb096e49180c5664946dc5a27434e7f59c13
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-01-12 10:05:55 +0100

    set tensors as class attribute, add logs

--- mlap/structure.py
+++ mlap/structure.py
@@ -1,13 +1,14 @@
 
-from.element import ElementMap
-from.neighbor import Neighbor
-from typing import TextIO, Tuple, List, Dict
+from .logger import logger
+from .element import ElementMap
+from .neighbor import Neighbor
+from typing import List, Dict
 from collections import defaultdict
 import torch
 
-
 dtype = torch.double
 device = torch.device("cpu")
+
 
 class Structure:
   """
@@ -20,18 +21,21 @@ class Structure:
     """
     self._data = data
     self._tensors = defaultdict(None)
-    self._element_map = None
-    self._neighbor = Neighbor(r_cutoff=12.0)
-
+    self.element_map = None
+    self.neighbor = Neighbor(r_cutoff=12.0) # TODO: cutoff value from descriptor
+    
+    # Tensors
     self._cast_to_tensors()
-    # self._neighbor.build(self)
+    self._set_tensors_as_attr()
+    
+    # Neighbor atoms
+    self.neighbor.build(self)
 
   def _cast_to_tensors(self):
     """
     Cast structure data into the (pytorch) tensors.
     TODO: check the dictionary for missing items
     TODO: take care of some missing items.
-    TODO: logging
     """
     # Direct casting
     self._tensors["position"] = torch.tensor(self._data["position"], dtype=dtype, device=device, requires_grad=True)
@@ -41,13 +45,22 @@ class Structure:
     self._tensors["cell"] = torch.tensor(self._data["cell"], dtype=dtype, device=device)
 
     # Set atom types using element mapping
-    self._element_map = ElementMap(self._data["element"])
-    atom_type = [self._element_map[elem] for elem in self._data["element"]] # TODO: optimize?
-    self._tensors["atom_type"] = torch.tensor(atom_type, dtype=torch.int, device=device)
+    self.element_map = ElementMap(self._data["element"])
+    atom_type = [self.element_map[elem] for elem in self._data["element"]] # TODO: optimize?
+    self._tensors["atom_type"] = torch.tensor(atom_type, dtype=torch.long, device=device)
+
+    for name, tensor in self._tensors.items():
+      logger.info(
+        f"Allocated '{name}' as a Tensor(shape='{tensor.shape}', dtype='{tensor.dtype}', device='{tensor.device}')")
+
+  def _set_tensors_as_attr(self):
+    logger.info(f"Setting {len(self._tensors)} tensors as attributes: {', '.join(self._tensors.keys())}")
+    for name, tensor in self._tensors.items():
+      setattr(self, name, tensor)
       
-  def _cast_to_data(self):
+  def _cast_to_data(self) -> Dict[str, List]:
     """
-    Cast the tensors to structure data (dictionary). 
+    Cast the tensors to structure data. 
     """
     pass
 
