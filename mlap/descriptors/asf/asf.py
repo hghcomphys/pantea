86b1e773f32b8e7e01c16aeb519569e2ba3b700d
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-01-16 16:47:14 +0100

    add test radial ASF terms

--- mlap/descriptors/asf/asf.py
+++ mlap/descriptors/asf/asf.py
@@ -3,6 +3,7 @@ from ...logger import logger
 from ...structure import Structure
 from ..base import Descriptor
 from .cutoff_function import CutoffFunction
+from .radial import G1, G2
 import torch
 
 dtype = torch.double
@@ -25,11 +26,16 @@ class ASF (Descriptor):
     """
     self.r_cutoff = structure.neighbor.r_cutoff  # has to be set durning class instantiation
     self.cutoff_function= CutoffFunction(self.r_cutoff, "tanh")
+    self.cutoff_type = self.cutoff_function.fn_type
 
     x = structure.position
     at = structure.atom_type
     nn  = structure.neighbor.numbers
     ngb = structure.neighbor.indices
+    
+    self._terms["G1"] = G1(self.r_cutoff, self.cutoff_type)
+    self._terms["G2"] = G2(self.r_cutoff, self.cutoff_type, r_shift=0.0, eta=0.3)
+    result = torch.zeros(len(self._terms), dtype=dtype, device=device)
 
     # Get the list of neighboring atom indices
     ni_ = ngb[aid, :nn[aid]]
@@ -39,15 +45,10 @@ class ASF (Descriptor):
     # Find the neighboring atom indices which match the given ASF cutoff and atom type
     ngb_rc_ = (rij < self.r_cutoff).detach()
     ngb_ = torch.nonzero(torch.logical_and(ngb_rc_, tij == 1), as_tuple=True)[0]
-    print(tij)
     # Apply the ASF term and sum over neighboring atoms
-    result = torch.sum(self.kernel(rij[ngb_]), dim=0)
+    for i, descriptor in enumerate(self._terms.values()):
+      result[i] = torch.sum( descriptor.kernel(rij[ngb_] ), dim=0)
 
     return result
-
-  def kernel(self, rij: torch.tensor) -> torch.tensor:
-    # TODO: improve design for kernel arguments
-    return torch.exp(-rij**2) * self.cutoff_function(rij) #  torch.tanh(1.0 - rij/self.r_cutoff).pow(3)
-
 
 
