28259a1d83818dfca8d8a97c8d3376ff389eda82
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-01-16 15:49:54 +0100

    optimize loop over descriptor kernels using torch methods

--- mlap/descriptors/asf/asf.py
+++ mlap/descriptors/asf/asf.py
@@ -15,31 +15,39 @@ class ASF (Descriptor):
   ASF is vector of different radial and angular terms.
   """
 
-  def __init__(self):
+  def __init__(self, element: str= None):
+    self.element = element
     self._terms = {}
 
   def __call__(self, structure:Structure, aid: int) -> torch.tensor: 
     """
     Calculate descriptor values for the input given structure.
     """
+    self.r_cutoff = structure.neighbor.r_cutoff  # has to be set durning class instantiation
+    self.cutoff_function= CutoffFunction(self.r_cutoff, "tanh")
+
     x = structure.position
+    at = structure.atom_type
     nn  = structure.neighbor.numbers
     ngb = structure.neighbor.indices
-    self.r_cutoff = structure.neighbor.r_cutoff  # has to be set durning class instantiation
-    self._cutoff_func= CutoffFunction(self.r_cutoff, "tanh")
 
-    # Loop over neighbors
-    # TODO: optimize
-    result = torch.tensor(0.0, dtype=torch.float)
-    rij = torch.norm(x[ngb[aid, :nn[aid]]]-x[0], dim=1)
-    neighbors_ = torch.nonzero( rij < self.r_cutoff, as_tuple=True)[0]
-    for nb in neighbors_.detach().tolist():
-        result = result + self.kernel(rij[nb])
+    # Get the list of neighboring atom indices
+    ni_ = ngb[aid, :nn[aid]]
+    # Calculate the distances of neighboring atoms and the corresponding atom types
+    rij = torch.norm(x[ni_]-x[0], dim=1)
+    tij = at[ni_] 
+    # Find the neighboring atom indices which match the given ASF cutoff and atom type
+    ngb_rc_ = (rij < self.r_cutoff).detach()
+    ngb_ = torch.nonzero(torch.logical_and(ngb_rc_, tij == 1), as_tuple=True)[0]
+    print(tij)
+    # Apply the ASF term and sum over neighboring atoms
+    result = torch.sum(self.kernel(rij[ngb_]), dim=0)
+
     return result
 
   def kernel(self, rij: torch.tensor) -> torch.tensor:
     # TODO: improve design for kernel arguments
-    return torch.exp(-rij**2) * self._cutoff_func(rij) #  torch.tanh(1.0 - rij/self.r_cutoff).pow(3)
+    return torch.exp(-rij**2) * self.cutoff_function(rij) #  torch.tanh(1.0 - rij/self.r_cutoff).pow(3)
 
 
 
