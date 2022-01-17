f7ef21cb99cc65ab0c214a86bb0333b25a1e94a4
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-01-17 10:06:55 +0100

    add add() method, minor improvements

--- mlap/descriptors/asf/asf.py
+++ mlap/descriptors/asf/asf.py
@@ -1,9 +1,11 @@
 from collections import defaultdict
+from mlap.descriptors.asf.angular import AngularSymmetryFunction
 from ...logger import logger
 from ...structure import Structure
 from ..base import Descriptor
 from .cutoff_function import CutoffFunction
-from .radial import G1, G2
+from .radial import G1, G2, RadialSymmetryFunction
+from typing import Union
 import torch
 
 dtype = torch.double
@@ -14,40 +16,56 @@ class ASF (Descriptor):
   """
   Atomic Symmetry Function (ASF) descriptor.
   ASF is vector of different radial and angular terms.
+  # TODO: ASF should be independent of the structure 
   """
 
-  def __init__(self, element: str= None):
+  def __init__(self, element: str) -> None:
     self.element = element
-    self._terms = {}
+    self._radial = []
+    self._angular = []
+    # TODO: read from input.nn
+
+  def add(self, asf: Union[RadialSymmetryFunction,  AngularSymmetryFunction],
+                neighbor_element1: str, 
+                neighbor_element2: str = None) -> None:
+    """
+    This method adds an input radial symmetry function to the list of ASFs.
+    # TODO: tuple of dict? (tuple is fine if it's used internally)
+    """
+    if isinstance(asf, RadialSymmetryFunction):
+      self._radial.append((asf, self.element, neighbor_element1))
+    elif isinstance(asf, RadialSymmetryFunction):
+      self._angular((asf, self.element, neighbor_element1, neighbor_element2))
+    else:
+      msg = f"Unknown input symmetry function object type"
+      logger.error(msg)
+      raise TypeError(msg)
 
   def __call__(self, structure:Structure, aid: int) -> torch.tensor: 
     """
     Calculate descriptor values for the input given structure.
     """
-    self.r_cutoff = structure.neighbor.r_cutoff  # has to be set durning class instantiation
-    self.cutoff_function= CutoffFunction(self.r_cutoff, "tanh")
-    self.cutoff_type = self.cutoff_function.fn_type
-
     x = structure.position
     at = structure.atom_type
     nn  = structure.neighbor.numbers
     ngb = structure.neighbor.indices
-    
-    self._terms["G1"] = G1(self.r_cutoff, self.cutoff_type)
-    self._terms["G2"] = G2(self.r_cutoff, self.cutoff_type, r_shift=0.0, eta=0.3)
-    result = torch.zeros(len(self._terms), dtype=dtype, device=device)
+    emap= structure.element_map
+
+    result = torch.zeros(len(self._radial), dtype=dtype, device=device)
 
     # Get the list of neighboring atom indices
     ni_ = ngb[aid, :nn[aid]]
     # Calculate the distances of neighboring atoms and the corresponding atom types
-    rij = torch.norm(x[ni_]-x[0], dim=1)
+    # TODO: apply PBC
+    rij = torch.norm(x[ni_]-x[0], dim=1) 
     tij = at[ni_] 
-    # Find the neighboring atom indices which match the given ASF cutoff and atom type
-    ngb_rc_ = (rij < self.r_cutoff).detach()
-    ngb_ = torch.nonzero(torch.logical_and(ngb_rc_, tij == 1), as_tuple=True)[0]
-    # Apply the ASF term and sum over neighboring atoms
-    for i, descriptor in enumerate(self._terms.values()):
-      result[i] = torch.sum( descriptor.kernel(rij[ngb_] ), dim=0)
+    # Loop of radial terms
+    for i, g in enumerate(self._radial):
+      # Find the neighboring atom indices that match the given ASF cutoff radius and atom type
+      ngb_rc_ = (rij < g[0].r_cutoff ).detach()
+      ngb_ = torch.nonzero(torch.logical_and(ngb_rc_, tij == emap[g[2]]), as_tuple=True)[0]
+      # Apply the ASF term and sum over the neighboring atoms
+      result[i] = torch.sum( g[0].kernel(rij[ngb_] ), dim=0)
 
     return result
 
