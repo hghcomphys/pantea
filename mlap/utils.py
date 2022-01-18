60767a695c58a12ab66c8e67d64fa8f30962f202
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-01-18 11:43:37 +0100

    add read for structure loader

--- mlap/utils.py
+++ mlap/utils.py
@@ -1,4 +1,20 @@
+from .logger import logger
+from .structure import Structure
+from .loader import StructureLoader
+from typing import List
 import torch
+
+
+def read_structures(structure_loader: StructureLoader) -> List[Structure]:
+    """
+    Read the input structure loader and return a list of structures.
+    """
+    structures = []
+    # Loop over structure data
+    for index, data in enumerate(structure_loader.get_data(), start=1):
+        logger.info(f"Reading structure {index}")
+        structures.append( Structure(data) )   
+    return structures  
 
 
 def gradient(y, x, grad_outputs=None):
