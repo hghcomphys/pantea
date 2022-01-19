019199ee0c5e5fbdc781c1ecbc1445da51fd6600
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-01-19 09:57:52 +0100

    rename cell, check lattice dimension

--- mlap/box.py
+++ mlap/box.py
@@ -1,3 +1,4 @@
+from .logger import logger
 import torch
 
 dtype = torch.double
@@ -5,16 +6,22 @@ device = torch.device("cpu")
 
 class Box:
   """
-  A class which contains cell info.
-  Currently, it only works for orthogonal cell.
+  A class which contains lattice info.
+  Currently, it only works for orthogonal lattice.
   """
-  def __init__(self, cell):
+  def __init__(self, lattice: torch.Tensor) -> None:
     """
     Initialize box.
     """
+    # Check lattice matrix shape
+    if lattice.shape != (3, 3):
+      msg = f"Unexpected lattice dimension {lattice.shape}"
+      logger.error(msg)
+      raise ValueError(msg)
+
     self.xlo = torch.tensor(0.0, dtype=dtype, device=device)
     self.ylo = torch.tensor(0.0, dtype=dtype, device=device)
     self.zlo = torch.tensor(0.0, dtype=dtype, device=device)
-    self.xhi = cell[0, 0]
-    self.yhi = cell[1, 1]
-    self.zhi = cell[2, 2]
+    self.xhi = lattice[0, 0]
+    self.yhi = lattice[1, 1]
+    self.zhi = lattice[2, 2]
