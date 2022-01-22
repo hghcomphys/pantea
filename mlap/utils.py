c14a4cfc05c44c6fa9461c7afcb8c51c7f949e6a
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-01-22 12:01:12 +0100

    define between argument to read_structures(), add logging

--- mlap/utils.py
+++ mlap/utils.py
@@ -1,19 +1,24 @@
 from .logger import logger
 from .structure import Structure
 from .loaders import StructureLoader
-from typing import List
+from typing import List, Tuple
 import torch
 
 
-def read_structures(structure_loader: StructureLoader) -> List[Structure]:
+def read_structures(structure_loader: StructureLoader, between: Tuple[int, int]=None) -> List[Structure]:
     """
     Read the input structure loader and return a list of structures.
     """
     structures = []
+    count = 0
     # Loop over structure data
     for index, data in enumerate(structure_loader.get_data(), start=1):
-        logger.info(f"Reading structure {index}")
+        count += 1
+        if (between is not None) and ( (index < between[0]) or (index > between[1]) ):
+            continue
+        logger.info(f"Reading structure #{index}")
         structures.append( Structure(data) )   
+    logger.info(f"Read {len(structures)} of {count} structures")
     return structures  
 
 
