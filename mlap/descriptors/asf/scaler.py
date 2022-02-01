717816ade0f1898b3d3fc20b1d88d76fa1a181b7
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-02-01 09:55:58 +0100

    ideas for scaler class design (in progeress)

--- mlap/descriptors/asf/scaler.py
+++ mlap/descriptors/asf/scaler.py
@@ -8,16 +8,54 @@ import torch
 class AtomicSymmetryFunctionScaler:
   """
   Scale descriptor values.
+  TODO: see https://notmatthancock.github.io/2017/03/23/simple-batch-stat-updates.html
   """
-  def __init__(self, **kwargs):
-    self.min = defaultdict(np.inf)
-    self.max = defaultdict(-np.inf)
-    self.mean = defaultdict(float)
-    self.sigma = defaultdict(float)
+  def __init__(self, element: str, **kwargs):
+    self.min = np.inf
+    self.max = -np.inf
+    self.sum = 0.0
+    self.num = 0
+    self.smin = kwargs.get("smin", 0.0)
+    self.smax = kwargs.get("smax", 1.0)
   
-  def fit(self, values: Dict[str, torch.Tensor]) -> None:
+  def fit(self, descriptor_values: torch.Tensor) -> None:
+    """
+    This method extract stattical quantities from the input descriptor values.
+    """
+
+    for element in descriptor_values:
+
+      val = descriptor_values[element].detach()
+
+      min_ = torch.min(val).numpy()
+      if min_ < self.min[element]:
+        self.min[element] = min_
+
+      max_ = torch.max(val).numpy()
+      if max_ > self.max[element]:
+        self.max[element] = max_
+
+      self.sum[element] += torch.sum(val).numpy()
+      self.num[element] += val.shape[0]
+
+   
+  def transform(self, descriptor_values: Dict[str, torch.Tensor]):
     pass
 
-  def transform(self, values: Dict[str, torch.Tensor]):
-    pass
+  def _center(self, G: torch.Tensor, element: str) -> torch.Tensor:
+      mean_ = self.sum[element] / self.num[element]
+      return G - mean_
+
+  def _scale(self, G: torch.Tensor, element: str) -> torch.Tensor:
+      return self.smin + (self.smax - self.smin) * (G - self.min[element]) / (self.max[element]- self.min[element])
+
+  def _scalecenter(self, G: torch.Tensor, element: str) -> torch.Tensor:
+      mean_ = self.sum[element] / self.num[element]
+      return self.smin + (self.smax - self.smin) * (G - mean_) / (self.max[element]- self.min[element])
+  
+  def _scalesigma(self, G: torch.Tensor, element: str) -> torch.Tensor:
+      mean_ = self.sum[element] / self.num[element]
+      sigma_ = None # TODO
+      return self.smin + (self.smax - self.smin) * (G - mean_) / sigma_
+
 
