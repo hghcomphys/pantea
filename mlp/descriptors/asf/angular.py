e26a6be47d9e43a952c8ba289dd6400930c39de1
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-02-20 11:53:54 +0100

    change input argument to cutoff function instance, improve logging message (debug)

--- mlp/descriptors/asf/angular.py
+++ mlp/descriptors/asf/angular.py
@@ -1,33 +1,42 @@
 
+from ...logger import logger
 from .cutoff import CutoffFunction
 import math
 import torch
+
 
 class AngularSymmetryFunction:
   """
   Three body symmetry function.
   TODO: add other variant of angular symmetry functions.
   """
-  def __init__(self, r_cutoff: float, cutoff_type: str):
-    self.r_cutoff = r_cutoff
-    self.cutoff_type = cutoff_type
-    self.cutoff_function = CutoffFunction(r_cutoff, cutoff_type)
+  def __init__(self, cutoff_function: CutoffFunction):
+    self.cutoff_function = cutoff_function
+    logger.debug(repr(self))
 
   def kernel(self, rij: torch.tensor) -> torch.tensor:
     raise NotImplementedError
+
+  def __repr__(self) -> str:
+    return f"{self.__class__.__name__}(" + \
+      ', '.join(['{}={!r}'.format(k, v) for k, v in self.__dict__.items() if "_"]) + ")"
+
+  @property
+  def r_cutoff(self) -> float:
+    return self.cutoff_function.r_cutoff
 
 
 class G3(AngularSymmetryFunction):
   """
   Angular symmetry function.
   """
-  def __init__(self, r_cutoff: float, cutoff_type: str, eta: float, zeta: float, lambda0: float, r_shift: float) -> None:
-    super().__init__(r_cutoff, cutoff_type)
+  def __init__(self, cutoff_function: CutoffFunction, eta: float, zeta: float, lambda0: float, r_shift: float) -> None:
     self.eta = eta
     self.zeta = zeta
     self.lambda0 = lambda0
     self.r_shift = r_shift
     self._scale_factor = math.pow(2.0, 1.0-self.zeta)
+    super().__init__(cutoff_function)
     
   def kernel(self, rij: torch.tensor, rik: torch.tensor, rjk: torch.tensor, cost: torch.tensor) -> torch.tensor:
     res = self._scale_factor * torch.pow(1 + self.lambda0 * cost, self.zeta) * torch.exp( -self.eta * (rij**2 + rik**2 + rjk**2) )  # TODO: r_shift
@@ -39,13 +48,13 @@ class G9(AngularSymmetryFunction):
   Modified angular symmetry function.
   Ref -> J. Behler, J. Chem. Phys. 134, 074106 (2011)
   """
-  def __init__(self, r_cutoff: float, cutoff_type: str, eta: float, zeta: float, lambda0: float, r_shift: float) -> None:
-    super().__init__(r_cutoff, cutoff_type)
+  def __init__(self, cutoff_function: CutoffFunction, eta: float, zeta: float, lambda0: float, r_shift: float) -> None:
     self.eta = eta
     self.zeta = zeta
     self.lambda0 = lambda0
     self.r_shift = r_shift
     self._scale_factor = math.pow(2.0, 1.0-self.zeta)
+    super().__init__(cutoff_function)
     
   def kernel(self, rij: torch.tensor, rik: torch.tensor, rjk: torch.tensor, cost: torch.tensor) -> torch.tensor:
     res = self._scale_factor * torch.pow(1 + self.lambda0 * cost, self.zeta) * torch.exp( -self.eta * (rij**2 + rik**2) ) # TODO: r_shift
--- mlp/descriptors/asf/radial.py
+++ mlp/descriptors/asf/radial.py
@@ -1,3 +1,4 @@
+from ...logger import logger
 from .cutoff import CutoffFunction
 import torch
 
@@ -11,21 +12,27 @@ class RadialSymmetryFunction:
   TODO: add other variant of radial symmetry functions.
   TODO: add logging when initializing each symmetry function.
   """
-  def __init__(self, r_cutoff: float, cutoff_type: str):
-    self.r_cutoff = r_cutoff
-    self.cutoff_type = cutoff_type
-    self.cutoff_function = CutoffFunction(r_cutoff, cutoff_type)
+  def __init__(self, cutoff_function: CutoffFunction):
+    self.cutoff_function = cutoff_function
+    logger.debug(repr(self))
 
   def kernel(self, rij: torch.tensor) -> torch.tensor:
     raise NotImplementedError
+
+  def __repr__(self) -> str:
+    return f"{self.__class__.__name__}(" + ', '.join(['{}={!r}'.format(k, v) for k, v in self.__dict__.items()]) + ")"
+
+  @property
+  def r_cutoff(self) -> float:
+    return self.cutoff_function.r_cutoff
 
 
 class G1(RadialSymmetryFunction):
   """
   Plain cutoff function.
   """
-  def __init__(self, r_cutoff: float, cutoff_type: str):
-    super().__init__(r_cutoff, cutoff_type)
+  def __init__(self, cutoff_function: CutoffFunction):
+    super().__init__(cutoff_function)
 
   def kernel(self, rij: torch.tensor) -> torch.tensor:
     return self.cutoff_function(rij)
@@ -35,10 +42,10 @@ class G2(RadialSymmetryFunction):
   """
   Radial exponential term.
   """
-  def __init__(self, r_cutoff: float, cutoff_type: str, r_shift: float, eta: float):
-    super().__init__(r_cutoff, cutoff_type)
+  def __init__(self, cutoff_function: CutoffFunction, r_shift: float, eta: float):
     self.r_shift = r_shift
     self.eta = eta
+    super().__init__(cutoff_function)
 
   def kernel(self, rij: torch.tensor) -> torch.tensor:
     return torch.exp( -self.eta * (rij - self.r_shift)**2 ) * self.cutoff_function(rij)
