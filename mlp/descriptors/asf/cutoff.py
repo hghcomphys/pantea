d982330f4051ef21ab9a0d45dcb18a7358cb972a
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-02-24 08:49:34 +0100

    define inv_r_cutoff variable

--- mlp/descriptors/asf/cutoff.py
+++ mlp/descriptors/asf/cutoff.py
@@ -11,12 +11,14 @@ class CutoffFunction:
   TODO: add poly 3 & 4 funcions
   See N2P2 -> https://compphysvienna.github.io/n2p2/api/cutoff_functions.html?highlight=cutoff#
               https://compphysvienna.github.io/n2p2/topics/keywords.html?highlight=cutoff_type
+  TODO: define inv_r_cutoff
   """
   _TANH_PRE = math.pow((math.e + 1 / math.e) / (math.e - 1 / math.e), 3)
 
   def __init__(self, r_cutoff: float, cutoff_type: str = "tanh"):
     self.r_cutoff = r_cutoff
     self.cutoff_type = cutoff_type.lower()
+    self.inv_r_cutoff = 1.0 / self.r_cutoff
     # Set cutoff type function
     try:
       self.fn = getattr(self, f"_{self.cutoff_type}")
@@ -32,16 +34,16 @@ class CutoffFunction:
     return torch.ones_like(r)
 
   def _tanhu(self, r: torch.Tensor) -> torch.Tensor:
-    return torch.tanh(1.0 - r/self.r_cutoff).pow(3)
+    return torch.tanh(1.0 - r * self.inv_r_cutoff).pow(3)
   
   def _tanh(self, r: torch.Tensor) -> torch.Tensor:
-    return self._TANH_PRE * torch.tanh(1.0 - r/self.r_cutoff).pow(3)
+    return self._TANH_PRE * torch.tanh(1.0 - r * self.inv_r_cutoff).pow(3)
 
   def _cos(self, r: torch.Tensor) -> torch.Tensor:
-    return 0.5 * (torch.cos(math.pi * r/self.r_cutoff) + 1.0)
+    return 0.5 * (torch.cos(math.pi * r * self.inv_r_cutoff) + 1.0)
 
   def _exp(self, r: torch.Tensor) -> torch.Tensor:
-    return torch.exp(1.0 - 1.0 / (1.0 - (r/self.r_cutoff)**2) )
+    return torch.exp(1.0 - 1.0 / (1.0 - (r * self.inv_r_cutoff)**2) )
 
   def _poly1(self, r: torch.Tensor) -> torch.Tensor:
     return (2.0*r - 3.0) * r**2 + 1.0
