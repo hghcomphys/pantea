c9bb03fc00fd9930b2cc0050fca9331501e6e5f3
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-02-25 10:12:08 +0100

    add version, description, cuda availability info

--- mlp/__init__.py
+++ mlp/__init__.py
@@ -1,1 +1,18 @@
+"""Machine Learning Potential (MLP) framework is a generic and GPU-accelerated package 
+written in Python/C++ to facilitate the development of emerging machine learning 
+interatomic potentials. Such potentials are employed to perform large-scale molecular 
+dynamics simulations of complex materials in computational physics and chemistry."""
 
+from .logger import logger
+from .config import CFG
+
+
+__version__ = "0.0.1"
+
+# Version
+logger.info(f"{__doc__}")
+logger.info(f"MLP framework version: {__version__}")
+
+# CUDA availability
+logger.info(f"CUDA availability: {CFG['is_cuda']}")
+logger.info(f"Default device: '{CFG['device']}'")
