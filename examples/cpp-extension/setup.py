d4f755b1cab1e6907b4258644eca7821bf7c9864
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-03-10 10:47:03 +0100

    add an generic example on how to use torch C++ extension

---
+++ examples/cpp-extension/setup.py
@@ -0,0 +1,17 @@
+"""
+How to run:
+    $ python setup.py install
+    $ python lltm.py
+"""
+
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CppExtension
+
+setup(
+    name='lltm_cpp',
+    ext_modules=[
+        CppExtension('lltm_cpp', ['lltm.cpp']),
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    })
