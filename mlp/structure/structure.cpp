e34e528ab311dd9ddf539673ea96fe582c86d501
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-03-15 21:43:53 +0100

    define apply_pbc() based on broadcasting  directional _apply_pbc()

--- mlp/structure/structure.cpp
+++ mlp/structure/structure.cpp
@@ -4,6 +4,7 @@ using namespace torch::indexing;
 
 
 torch::Tensor _apply_pbc(torch::Tensor dx, torch::Tensor l) {
+  // Apply PBC in one direction
   // TODO: in-place dx?
   auto dx_ = torch::where(dx  >  0.5E0*l, dx  - l, dx );
        dx_ = torch::where(dx_ < -0.5E0*l, dx_ + l, dx_);
@@ -12,9 +13,10 @@ torch::Tensor _apply_pbc(torch::Tensor dx, torch::Tensor l) {
 
 torch::Tensor apply_pbc(torch::Tensor dx, torch::Tensor box) {
   // TODO: in-place dx?
-  for (int dim=0; dim<3; dim++)
-    dx.index_put_( {"...", dim}, apply_pbc(dx, box.index({dim})) );
-  return dx;
+  // for (int dim=0; dim<3; dim++)
+  //   dx.index_put_({"...", dim}, apply_pbc(dx, box.index({0})) );
+  // return dx;
+  return _apply_pbc(dx, box);
 }
 
 
