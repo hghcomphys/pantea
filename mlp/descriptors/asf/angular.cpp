2927b4f37fd19b401215afae8aaecb707199dc8a
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-03-20 10:04:11 +0100

    add G9 angular c++ kernel

--- mlp/descriptors/asf/angular.cpp
+++ mlp/descriptors/asf/angular.cpp
@@ -11,6 +11,17 @@ torch::Tensor g3_kernel(torch::Tensor rij,
 }
 
 
+torch::Tensor g9_kernel(torch::Tensor rij,
+                        torch::Tensor rik,
+                        torch::Tensor rjk,
+                        torch::Tensor cost,
+                        std::vector<double> params) {
+  return params[4] * torch::pow(1.0D + params[2] * cost, params[1]) * torch::exp( -params[0] * (rij*rij + rik*rik) ); // TODO: r_shift
+  // return res * self.cutoff_function(rij) * self.cutoff_function(rik) * self.cutoff_function(rjk)
+}
+
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("g3_kernel", &g3_kernel, "CPP G3 kernel");
+  m.def("g3_kernel", &g3_kernel, "CPP G3 kernel"); 
+  m.def("g9_kernel", &g9_kernel, "CPP G9 kernel"); 
 }
--- mlp/descriptors/asf/angular.py
+++ mlp/descriptors/asf/angular.py
@@ -34,19 +34,21 @@ class G3(AngularSymmetryFunction):
     return res * self.cutoff_function(rij) * self.cutoff_function(rik) * self.cutoff_function(rjk)
 
 
-class G9(AngularSymmetryFunction):
+class G9(G3): # AngularSymmetryFunction
   """
   Modified angular symmetry function.
   Ref -> J. Behler, J. Chem. Phys. 134, 074106 (2011)
   """
-  def __init__(self, cutoff_function: CutoffFunction, eta: float, zeta: float, lambda0: float, r_shift: float) -> None:
-    self.eta = eta
-    self.zeta = zeta
-    self.lambda0 = lambda0
-    self.r_shift = r_shift
-    self._scale_factor = math.pow(2.0, 1.0-self.zeta)
-    super().__init__(cutoff_function)
+  # def __init__(self, cutoff_function: CutoffFunction, eta: float, zeta: float, lambda0: float, r_shift: float) -> None:
+  #   self.eta = eta
+  #   self.zeta = zeta
+  #   self.lambda0 = lambda0
+  #   self.r_shift = r_shift
+  #   self._scale_factor = math.pow(2.0, 1.0-self.zeta)
+  #   self._params = [self.eta, self.zeta, self.lambda0, self.r_shift, self._scale_factor]
+  #   super().__init__(cutoff_function)
     
   def kernel(self, rij: torch.Tensor, rik: torch.Tensor, rjk: torch.Tensor, cost: torch.Tensor) -> torch.Tensor:
-    res = self._scale_factor * torch.pow(1 + self.lambda0 * cost, self.zeta) * torch.exp( -self.eta * (rij**2 + rik**2) ) # TODO: r_shift
+    #res = self._scale_factor * torch.pow(1 + self.lambda0 * cost, self.zeta) * torch.exp( -self.eta * (rij**2 + rik**2) ) # TODO: r_shift
+    res = angular_cpp.g9_kernel(rij, rik, rjk, cost, self._params)
     return res * self.cutoff_function(rij) * self.cutoff_function(rik)
