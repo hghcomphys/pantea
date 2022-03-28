f6f340fa2d059be0ffd8ec8424cc561c8f3c632f
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-03-28 09:51:38 +0200

    udpate network model

--- torchip/models/base.py
+++ torchip/models/base.py
@@ -1,5 +1,7 @@
 
-class BaseModel:
+from torch import nn
+
+class BaseModel (nn.Module):
   """
   A base class for all kinds of ML-based potential models.
   """
--- torchip/models/nn/nn.py
+++ torchip/models/nn/nn.py
@@ -5,21 +5,30 @@ from torch import nn
 from typing import Tuple
 
 
-class NeuralNetworkModel(BaseModel, nn.Module):
+class NeuralNetworkModel(BaseModel):
   """
   A neural network model which map descriptor values to energy and force (derivative).
   """
-  def __init__(self, input_size: int, layers: Tuple[Tuple[int, str]]):
+  def __init__(self, 
+    input_size: int, 
+    hidden_layers: Tuple[Tuple[int, str]], 
+    output_layer:Tuple[int, str] = (1, 'l')):
     super(NeuralNetworkModel, self).__init__()
 
     # Prepare stack of layers
     linear_stack = []
     in_size = input_size
-    for out_size, af_type in layers:
+    # Hidden layers
+    for out_size, af_type in hidden_layers:
       linear_stack.append( nn.Linear(in_size, out_size) )
       linear_stack.append( self._get_activation_function(af_type) )
       in_size = out_size
+    # Output layer
+    linear_stack.append( nn.Linear(in_size, output_layer[0]) )
+    linear_stack.append( self._get_activation_function(output_layer[1]) )
+    # Build a sequential model
     self.linear_stack = nn.Sequential(*linear_stack)
+    # TODO: add logging
 
   def _get_activation_function(self, type_: str):
     """
