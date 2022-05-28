9b256c0da5c4c7c72f60a3412a8032e4da6c9ad1
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-05-28 20:37:42 +0200

    add expected methods to base SructureDataset class

--- torchip/dataset/base.py
+++ torchip/dataset/base.py
@@ -4,6 +4,10 @@ from torch.utils.data import Dataset
 class StructureDataset (Dataset):
   """
   A base class for atomic data structure. 
-  All other atomic dataset must be derived from this base class.
+  All future atomic datasets must be derived from this class.
   """
-  pass
+  def __len__(self):
+    raise NotImplementedError
+
+  def __getitem__(self, index):
+    raise NotImplementedError
