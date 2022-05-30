df119b720e3db464e10f785cea615a52a35beff3
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-05-30 22:09:02 +0200

    add complete title

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'TorchIP'
+project = 'TorchIP - Torch Interatomic Potential'
 copyright = '2022, Hossein Ghorbanfekr'
 author = 'Hossein Ghorbanfekr'
 
