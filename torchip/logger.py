1da48bb8fa3ef904f04586f52fca0f1b8c63b31c
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-06-19 20:39:48 +0200

    add log file argument to Logger

--- torchip/logger.py
+++ torchip/logger.py
@@ -5,6 +5,7 @@ Setup logger including the console and the rotating file handlers.
 # from .config import CFG
 import sys
 import logging
+from pathlib import Path
 from logging.handlers import RotatingFileHandler
 
 
@@ -12,22 +13,32 @@ class Logger:
   """
   An utility logger class which allows more tweaks than the standard python logging.  
   """
-  def __init__(self, name="TorchIP", level=logging.WARNING):
-    self.logger = logging.getLogger(name) 
+  def __init__(self, level=logging.WARNING, filename=None):
+    self.logger = logging.getLogger("TorchIP") 
     self.level = level
-    
-    self.set_level(level)
+    self.handlers = []
+
     self._add_console_handler()
+    if filename:
+      self._add_file_handler(filename)
+   
+    for handler in logging.root.handlers:
+      logging.root.removeHandler(handler)
+    logging.basicConfig(level=logging.DEBUG, handlers=self.handlers)
+
+    # Avoiding matplotlib debug messages
+    logging.getLogger('matplotlib').setLevel(logging.WARNING)
+    logging.getLogger('matplotlib.font_manager').disabled = True
 
   def set_level(self, level) -> None:
     """
-    Set the global logging level.  
+    Set the console logging level.  
 
     Args:
         level (logging): INFO, WARNING, ERROR, and DEBUG
     """
     self.level = level
-    self.logger.setLevel(level)
+    logging.root.handlers[0].setLevel(self.level)
 
   def _add_console_handler(self) -> None:
     """
@@ -36,29 +47,29 @@ class Logger:
     """
     console_handler = logging.StreamHandler(sys.stderr)
     console_handler.setFormatter(logging.Formatter('%(message)s'))  # logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-    # console_handler.setLevel(level)
-    self.logger.addHandler(console_handler)
+    console_handler.setLevel(self.level)
+    self.handlers.append(console_handler)
 
-  # FIXME: file handler 
-  # def add_log_file(self, filename="debug.log") -> None:
-  #   """
-  #   Add a file handler to the logging.
-  #   Further fine-tune adjustments (e.g. formatting) on file handler can be applied here. 
+  def _add_file_handler(self, filename) -> None:
+    """
+    Add a file handler to the logging.
+    Further fine-tune adjustments (e.g. formatting) on file handler can be applied here. 
 
-  #   Args:
-  #       logfile (str, optional): Path to log file name. Defaults to "debug.log".
-  #       level (logging, optional): Logging level for the log file. Defaults to logging.DEBUG.
-  #   """
-  #   # File handler is also useful for multi-process logging.
-  #   file_handler = RotatingFileHandler(filename, 'a', 1e6, 3)
-  #   file_handler.setFormatter(logging.Formatter('%(levelname)-8s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p'))  #  [%(asctime)s] [%(threadName)-10s]  [%(processName)-10s] %(name)s 
-  #   # file_handler.setLevel(level)
-  #   self.logger.addHandler(file_handler)
+    Args:
+        logfile (str, optional): Path to log file name. Defaults to "debug.log".
+        level (logging, optional): Logging level for the log file. Defaults to logging.DEBUG.
+    """
+    # File handler is also useful for multi-process logging.
+    file_handler = RotatingFileHandler(str(Path(filename)), 'a', 1e6, 3)
+    file_handler.setFormatter(logging.Formatter('%(levelname)-8s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p'))  #  [%(asctime)s] [%(threadName)-10s]  [%(processName)-10s] %(name)s 
+    file_handler.setLevel(logging.DEBUG)
+    self.handlers.append(file_handler)
 
   def debug(self, msg, *args, **kwargs):
     self.logger.debug(msg, *args, **kwargs)
   
   def print(self, msg="", **kwargs):
+    # TODO: ignore printing if current level is >= INFO
     print(msg, **kwargs)
     self.info(msg)
 
@@ -72,9 +83,10 @@ class Logger:
     self.warning(msg, *args, **kwargs)
 
   def error(self, msg, *args, **kwargs):
+    # TODO: get exception type as input argument
     self.logger.error(msg, *args, **kwargs)
     raise Exception(msg)
 
 
 # Create a global logger object
-logger = Logger()
+logger = Logger() # filename="debug.log"
