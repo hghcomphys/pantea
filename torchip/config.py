f7ccc4a3d009a6df453d586a613acbde537e132b
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-07-04 10:53:14 +0200

    improve design for global configuration

--- torchip/config.py
+++ torchip/config.py
@@ -1,41 +1,66 @@
 from .logger import logger
-# from pathlib import Path
 from typing import Any
 import torch
 
 
-class CFG:
+class _CFG:
   """
-  A global configuration class of default values for variables within the framework.
+  A base configuration class of default values for global variables.
   """
   # TODO: circular import error between CFG & logger
   
-  __conf = {
-    "is_cuda": torch.cuda.is_available(),
-    "device" : torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
-    "dtype": torch.float,
-    "dtype_index": torch.long, 
-    # "logging_level": logging.INFO,
-    # "log_file": Path("mlap.log"),
+  _conf = {
+    # Global variables!
   }
-  __setters = list(__conf.keys()) # TODO: limit the setters?
- 
-  @staticmethod
-  def get(name) -> Any:
-    return CFG.__conf[name]
 
-  @staticmethod
-  def set(name, value) -> None:
-    if name in CFG.__setters:
-      logger.debug(f"Resetting default {name}: '{value}'")
-      CFG.__conf[name] = value
+  def __init__(self):
+    self._set_attributes()
+
+  def _set_attributes(self):
+    for atr in self._conf:
+      setattr(self, atr, self._conf[atr])
+  
+  def get(self, name) -> Any:
+    return self._conf[name]
+
+  def set(self, name, value) -> None:
+    if name in self._conf.keys():
+      logger.debug(f"Setting {name}: '{value}'")
+      self._conf[name] = value
     else:
-      msg = f"Name '{name}' not accepted in the global configuration"
-      logger.error(msg)
-      raise NameError(msg)
+      logger.error(f"Name '{name}' is not allowed!", exception=NameError)
 
-  def __class_getitem__(cls, name) -> Any:
-     return cls.get(name)
+  def __getitem__(self, name) -> Any:
+     return self.get(name)
+
+
+class DataType(_CFG):
+  """
+  A configuration class for the tensors' data type.
+  """
+  _conf = {
+    "FLOATX" : torch.float,
+    "INT"    : torch.long,
+    "UINT"   : torch.long,
+    "INDEX"  : torch.long,
+  }
+
+
+class Device(_CFG):
+  """
+  A configuration class for the tensors' device.
+  TODO: further adjustments regarding mutiple GPUs are possible from here.
+  """
+  _conf = {
+    "CPU" : torch.device('cpu'),
+    "GPU" : torch.device('cuda'),
+    "DEVICE" : torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+  }
+  
+
+# Create dtype and device configurations
+dtype = DataType()
+device = Device()
 
 
 class TaskClient:
