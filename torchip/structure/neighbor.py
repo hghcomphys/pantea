6fa965730b21493acbfa0cbc484b55aca4b4904f
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-07-22 11:26:59 +0200

    Neighbor: define create tensor method

--- torchip/structure/neighbor.py
+++ torchip/structure/neighbor.py
@@ -14,27 +14,46 @@ class Neighbor:
   """
   def __init__(self, r_cutoff: float): 
     self.r_cutoff = r_cutoff
-    self.tensors = {}
+    self.tensors = None
     logger.debug(f"{self.__class__.__name__}(r_cutoff={self.r_cutoff})")
+
+  def _create_tensors_from(self, structure):
+    """
+    Create neighbor list tensors (allocate memory) from the input structure.
+
+    :param structure: an instance of Structure
+    :type structure: Dict[Tensor]
+    """
+    # TODO: reduce natoms*natoms tensor size!
+    # TODO: define max_num_neighbor to avoid extra memory allocation!
+    tensors_ = {}
+
+    # Neighbor atoms numbers and indices
+    tensors_["number"] = torch.empty(structure.natoms, dtype=dtype.UINT, device=structure.device)
+    tensors_["index"] = torch.empty(structure.natoms, structure.natoms, dtype=dtype.INDEX, device=structure.device) 
+    
+    # Logging
+    for name, tensor in tensors_.items():
+      logger.debug(
+        f"Allocated '{name}' as a Tensor(shape='{tensor.shape}', dtype='{tensor.dtype}', device='{tensor.device}')"
+      )
+
+    return tensors_
 
   def update(self, structure) -> None:
     """
     This method updates the neighbor atom tensors including the number of neighbor and neighbor atom indices 
     within the input structure.
-    
-    TODO: optimize updating the neighbor list, for example using the cell mesh, bin atoms (miniMD), etc.
-    TODO: reduce natoms*natoms tensor size!
-    TODO: define max_num_neighbor to avoid extra memory allocation!
     """    
+    #  TODO: optimize updating the neighbor list, for example using the cell mesh, bin atoms (miniMD), etc.
+
     if not structure.requires_neighbor_update:
       logger.debug("Skip updating the neighbor list")
       return
 
-    # Neighbor atoms numbers and indices
-    self.tensors["number"] = torch.empty(structure.natoms, dtype=dtype.UINT, device=structure.device)
-    self.tensors["index"] = torch.empty(structure.natoms, structure.natoms, dtype=dtype.INDEX, device=structure.device) 
+    self.tensors = self._create_tensors_from(structure)
     set_as_attribute(self, self.tensors)
-
+    
     nn = self.number
     ni = self.index
     for aid in range(structure.natoms): # TODO: optimization: torch unbind or vmap
