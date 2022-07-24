de6a17b75ff333d4935617ca3ec30ea32b773bd2
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-07-24 18:58:55 +0200

    improve Box, now it accepts any input lattice matrix

--- torchip/structure/box.py
+++ torchip/structure/box.py
@@ -13,20 +13,33 @@ class Box:
   TODO: box variables as numpy or pytorch?
   TODO: triclinic lattice
   """
-  def __init__(self, lattice: Tensor) -> None:
-    
-    # Check lattice matrix shape
-    if lattice.shape != (3, 3):
-      logger.error(f"Unexpected lattice dimension {lattice.shape}", exception=ValueError)
+  def __init__(self, 
+      lattice,
+      dtype: torch.dtype = None,
+      device: torch.device = None,
+    ) -> None:
+    """
+    Initialize the simulation box (super-cell).
 
-    self.lattice = lattice
-    kwargs = { "dtype": dtype.FLOATX, "device": device.DEVICE }
-    self.xlo = torch.tensor(0.0, **kwargs)
-    self.ylo = torch.tensor(0.0, **kwargs)
-    self.zlo = torch.tensor(0.0, **kwargs)
-    self.xhi = lattice[0, 0]
-    self.yhi = lattice[1, 1]
-    self.zhi = lattice[2, 2]
+    :param lattice: Lattice matrix (3x3 array)
+    :param dtype: Data type of internal tensors which represent structure, defaults to None
+    :type dtype: torch.dtype, optional
+    :param device: Device on which tensors are allocated, defaults to None
+    :type device: torch.device, optional
+    """    
+
+    self.lattice = torch.tensor(lattice, dtype=dtype, device=device)
+
+    # Check lattice matrix shape
+    if self.lattice.shape != (3, 3):
+      logger.error(f"Unexpected lattice dimension {self.lattice.shape}", exception=ValueError)
+
+    self.xlo = torch.tensor(0.0, dtype=dtype, device=device)
+    self.ylo = torch.tensor(0.0, dtype=dtype, device=device)
+    self.zlo = torch.tensor(0.0, dtype=dtype, device=device)
+    self.xhi = self.lattice[0][0]
+    self.yhi = self.lattice[1][1]
+    self.zhi = self.lattice[2][2]
 
   @property
   def lx(self) -> Tensor:
