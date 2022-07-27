afee11645271e73e2d8dbca43c7af06ff3e3620a
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-07-27 11:43:25 +0200

    update docstring

--- torchip/__init__.py
+++ torchip/__init__.py
@@ -1,12 +1,15 @@
 """
-TorchIP is a generic and GPU-accelerated framework written in Python/C++ to facilitate 
-the development of emerging machine learning interatomic potentials. TorchIP is intended 
-to help researchers to construct potentials that are employed to perform large-scale 
-molecular dynamics simulations of complex materials in computational physics and chemistry.
+TorchIP - PyTorch Interatomic Potential
+=======================================
 
-The core implementation of TorchIP is based on the PyTorch package and its C++ API which 
+TorchIP is a generic and GPU-accelerated software framework written in Python/C++ to facilitate 
+the development of emerging machine learning interatomic potentials. TorchIP is intended to help 
+researchers to construct ML-based potentials that are employed to perform large-scale molecular 
+dynamics simulations of complex materials in computational physics and chemistry.
+
+The core implementation of TorchIP is based on the PyTorch package, and its C++ API, which 
 provides two high-level features including optimized tensor computation and automatic 
-differentiation
+differentiation.
 """
 
 from .logger import logger
@@ -16,4 +19,4 @@ from .config import *
 __version__ = "0.5.1"
 
 logger.debug(f"{__doc__}")
-logger.debug(f"TorchIP {__version__}")
+logger.debug(f"Version: {__version__}")
