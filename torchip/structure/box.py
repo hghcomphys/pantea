f15a98938e20fc8480c302564d82df2e4d2863bc
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-07-29 15:34:37 +0200

    add apply_pbc kernels, improve class attributes

--- torchip/structure/box.py
+++ torchip/structure/box.py
@@ -13,11 +13,12 @@ class Box:
   TODO: box variables as numpy or pytorch?
   TODO: triclinic lattice
   """
-  def __init__(self, 
-      lattice,
-      dtype: torch.dtype = None,
-      device: torch.device = None,
-    ) -> None:
+  def __init__(
+    self, 
+    lattice,
+    dtype: torch.dtype = None,
+    device: torch.device = None,
+  ) -> None:
     """
     Initialize the simulation box (super-cell).
 
@@ -27,19 +28,66 @@ class Box:
     :param device: Device on which tensors are allocated, defaults to None
     :type device: torch.device, optional
     """    
-
     self.lattice = torch.tensor(lattice, dtype=dtype, device=device)
 
-    # Check lattice matrix shape
+    # Check lattice dimension
     if self.lattice.shape != (3, 3):
       logger.error(f"Unexpected lattice dimension {self.lattice.shape}", exception=ValueError)
 
-    self.xlo = torch.tensor(0.0, dtype=dtype, device=device)
-    self.ylo = torch.tensor(0.0, dtype=dtype, device=device)
-    self.zlo = torch.tensor(0.0, dtype=dtype, device=device)
-    self.xhi = self.lattice[0][0]
-    self.yhi = self.lattice[1][1]
-    self.zhi = self.lattice[2][2]
+  @staticmethod
+  def _apply_pbc(dx: Tensor, lattice: Tensor) -> Tensor:
+    """
+    [Serializable Kernel]
+    Apply the periodic boundary condition (PBC) along x,y, and z directions.
+
+    :param dx: Atomic position
+    :type dx: Tensor
+    :param lattice: lattice matrix
+    :type lattice: Tensor
+    :return: PBC applied position
+    :rtype: Tensor
+    """    
+    # TODO: using broadcasting for lattice?
+    for i in range(3):
+      l = lattice[i, i]
+      dx[..., i] = torch.where(dx[..., i] >  0.5E0*l, dx[..., i] - l, dx[..., i])
+      dx[..., i] = torch.where(dx[..., i] < -0.5E0*l, dx[..., i] + l, dx[..., i])
+    return dx
+
+  def apply_pbc(self, dx: Tensor) -> Tensor:
+    """
+    Apply the periodic boundary condition (PBC) on input tensor.
+
+    :param dx: Position difference
+    :type dx: Tensor
+    :return: PBC applied position difference
+    :rtype: Tensor
+    """    
+    return Box._apply_pbc(dx, self.lattice)
+
+  @property
+  def xlo(self) -> Tensor:
+    return torch.tensor(0.0, dtype=dtype, device=device)
+
+  @property
+  def ylo(self) -> Tensor:
+    return torch.tensor(0.0, dtype=dtype, device=device)
+
+  @property
+  def zlo(self) -> Tensor:
+    return torch.tensor(0.0, dtype=dtype, device=device)
+
+  @property
+  def xhi(self) -> Tensor:
+    return self.lattice[0, 0]
+
+  @property
+  def yhi(self) -> Tensor:
+    return self.lattice[1, 1]
+
+  @property
+  def zhi(self) -> Tensor:
+    return self.lattice[2, 2]
 
   @property
   def lx(self) -> Tensor:
