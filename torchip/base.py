7f7708798c6c60d5d7a4f21e6ac7bb48bc7cff30
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-09-09 10:19:16 +0200

    define a global base class for all torcihp classes

---
+++ torchip/base.py
@@ -0,0 +1,12 @@
+class BaseTorchipClass:
+  """
+  A base class which all other classes have to be derived from this one. 
+  It's basically intended to be used for setting the global properties or methods 
+  which expected to exist in all child classes.   
+  """
+  def __repr__(self) -> str:
+    return "{C}({attrs})".format(  # @{id:x}
+        C=self.__class__.__name__,
+        #id=id(self) & 0xFFFFFF,
+        attrs=", ".join("{}={!r}".format(k, v) for k, v in self.__dict__.items() if not k.startswith("_")),
+      )
