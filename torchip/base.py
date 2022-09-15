bf22b8830b8c0ed95e42513efdaea818e2044055
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-09-15 23:13:52 +0200

    add debug init message to global base class

--- torchip/base.py
+++ torchip/base.py
@@ -1,9 +1,14 @@
+from .logger import logger
+
 class BaseTorchipClass:
   """
   A base class which all other classes have to be derived from this one. 
   It's basically intended to be used for setting the global properties or methods 
   which expected to exist in all child classes.   
   """
+  def __init__(self):
+    logger.debug(f"Initializing {self}")
+
   def __repr__(self) -> str:
     return "{C}({attrs})".format(  # @{id:x}
         C=self.__class__.__name__,
