8c49d453efc1ad5488fdb5a313b08999b7009027
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-10-26 20:25:04 +0200

    replace math lib

--- torchip/descriptors/asf/cutoff.py
+++ torchip/descriptors/asf/cutoff.py
@@ -18,7 +18,7 @@ class CutoffFunction(_Base):
     TODO: define inv_r_cutoff
     """
 
-    _TANH_PRE = math.pow((math.e + 1 / math.e) / (math.e - 1 / math.e), 3)
+    _TANH_PRE = torch.pow((torch.e + 1 / torch.e) / (torch.e - 1 / torch.e), 3)
 
     def __init__(self, r_cutoff: float, cutoff_type: str = "tanh"):
         self.r_cutoff = r_cutoff
