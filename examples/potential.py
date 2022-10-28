1de3be3dba7dfe51c917cfe00441f9d0517939d8
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-10-28 21:53:01 +0200

    checkpoint: rename and update potential example

--- examples/potential.py
+++ examples/potential.py
@@ -1,30 +1,25 @@
 # # TorchIP: Lennard-Jones potential
-# An example notebook that shows how to reconstruct a Lennard-Jones potential using 
-# high-dimensional neural network potential (HDNNP). 
+# An example notebook that shows how to reconstruct a Lennard-Jones potential using
+# high-dimensional neural network potential (HDNNP).
 
 # ### Imports
 
 import sys
-sys.path.append('../')
+
+sys.path.append("../")
 
 import torchip
-from torchip.datasets import RunnerStructureDataset, ToStructure
+from torchip.datasets import RunnerStructureDataset
 from torchip.potentials import NeuralNetworkPotential
-
 import torch
-import logging
 import pandas as pd
 from pathlib import Path
-
-# from dask.distributed import Client
-# from torchip.config import TaskClient
-# TaskClient.client = Client(memory_limit='4GB', n_workers=2, processes=True, threads_per_worker=2, dashboard_address=':8791')
 
 # torchip.set_logging_level(logging.DEBUG)
 torchip.manual_seed(2022)
 torchip.device.DEVICE = torch.device("cpu")
 
-potdir = Path('./LJ')
+potdir = Path("./LJ")
 
 # Dataset
 structures = RunnerStructureDataset(Path(potdir, "input.data"), persist=True)
