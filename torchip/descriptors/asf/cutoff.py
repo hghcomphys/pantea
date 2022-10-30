2e0fe29f360c31880810c9b3ad1cb3bc5068865c
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-10-30 13:36:22 +0100

    jax implementation of cutoff function

--- torchip/descriptors/asf/cutoff.py
+++ torchip/descriptors/asf/cutoff.py
@@ -1,54 +1,11 @@
 from ...logger import logger
 from ...base import _Base
-from torch import Tensor
-import torch
+import math
+import jax
+import jax.numpy as jnp
+from functools import partial
 
-
-@torch.jit.script
-def _apply_cutoff(cfn_r: Tensor, r: Tensor, r_cutoff: float):
-    return torch.where(r < r_cutoff, cfn_r, torch.zeros_like(r))
-
-
-@torch.jit.script
-def _hard(r: Tensor, r_cutoff: float):
-    cfn_r: Tensor = torch.ones_like(r)
-    return _apply_cutoff(cfn_r, r, r_cutoff)
-
-
-@torch.jit.script
-def _tanhu(r: Tensor, r_cutoff: float):
-    cfn_r: Tensor = torch.tanh(1.0 - r / r_cutoff) ** 3
-    return _apply_cutoff(cfn_r, r, r_cutoff)
-
-
-@torch.jit.script
-def _tanh(r: Tensor, r_cutoff: float, _TANH_PRE: float):
-    cfn_r: Tensor = _TANH_PRE * torch.tanh(1.0 - r / r_cutoff) ** 3
-    return _apply_cutoff(cfn_r, r, r_cutoff)
-
-
-@torch.jit.script
-def _cos(r: Tensor, r_cutoff: float):
-    cfn_r: Tensor = 0.5 * (torch.cos(torch.pi * r / r_cutoff) + 1.0)
-    return _apply_cutoff(cfn_r, r, r_cutoff)
-
-
-@torch.jit.script
-def _exp(r: Tensor, r_cutoff: float):
-    cfn_r: Tensor = torch.exp(1.0 - 1.0 / (1.0 - (r / r_cutoff) ** 2))
-    return _apply_cutoff(cfn_r, r, r_cutoff)
-
-
-@torch.jit.script
-def _poly1(r: Tensor, r_cutoff: float):
-    cfn_r: Tensor = (2.0 * r - 3.0) * r**2 + 1.0
-    return _apply_cutoff(cfn_r, r, r_cutoff)
-
-
-@torch.jit.script
-def _poly2(r: Tensor, r_cutoff: float):
-    cfn_r: Tensor = ((15.0 - 6.0 * r) * r - 10) * r**3 + 1.0
-    return _apply_cutoff(cfn_r, r, r_cutoff)
+Tensor = jnp.ndarray
 
 
 class CutoffFunction(_Base):
@@ -62,7 +19,7 @@ class CutoffFunction(_Base):
     TODO: define inv_r_cutoff
     """
 
-    _TANH_PRE = ((torch.e + 1 / torch.e) / (torch.e - 1 / torch.e)) ** 3
+    _TANH_PRE = ((math.e + 1 / math.e) / (math.e - 1 / math.e)) ** 3
 
     def __init__(self, r_cutoff: float, cutoff_type: str = "tanh"):
         self.r_cutoff = r_cutoff
@@ -76,29 +33,30 @@ class CutoffFunction(_Base):
                 exception=NotImplementedError,
             )
 
+    @partial(jax.jit, static_argnums=(0,))  # FIXME
     def __call__(self, r: Tensor) -> Tensor:
-        return self.cutoff_function(r)
+        return jnp.where(r < self.r_cutoff, self.cutoff_function(r), jnp.zeros_like(r))
 
     def hard(self, r: Tensor) -> Tensor:
-        return _hard(r, self.r_cutoff)
+        return jnp.ones_like(r)
 
     def tanhu(self, r: Tensor) -> Tensor:
-        return _tanhu(r, self.r_cutoff)
+        return jnp.tanh(1.0 - r / self.r_cutoff) ** 3
 
     def tanh(self, r: Tensor) -> Tensor:
-        return _tanh(r, self.r_cutoff, self._TANH_PRE)
+        return self._TANH_PRE * jnp.tanh(1.0 - r / self.r_cutoff) ** 3
 
     def cos(self, r: Tensor) -> Tensor:
-        return _cos(r, self.r_cutoff)
+        return 0.5 * (jnp.cos(jnp.pi * r / self.r_cutoff) + 1.0)
 
     def exp(self, r: Tensor) -> Tensor:
-        return _exp(r, self.r_cutoff)
+        return jnp.exp(1.0 - 1.0 / (1.0 - (r / self.r_cutoff) ** 2))
 
     def poly1(self, r: Tensor) -> Tensor:
-        return _poly1(r, self.r_cutoff)
+        return (2.0 * r - 3.0) * r**2 + 1.0
 
     def poly2(self, r: Tensor) -> Tensor:
-        return _poly2(r, self.r_cutoff)
+        return ((15.0 - 6.0 * r) * r - 10) * r**3 + 1.0
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(r_cutoff={self.r_cutoff}, cutoff_type='{self.cutoff_type}')"
