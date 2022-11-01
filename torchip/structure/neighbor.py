b48b7e29c43df5769a827d0e901fbbb21b260970
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-11-01 16:11:44 +0100

    introducing neighbor mask, using optimized jit and vmap kernels (jax)

--- torchip/structure/neighbor.py
+++ torchip/structure/neighbor.py
@@ -9,27 +9,34 @@ from functools import partial
 Tensor = jnp.ndarray
 
 
-# TODO: jit
-# @partial(jax.jit, static_argnums=(0,))  # FIXME
-def _update(
-    structure,
-    nn: Tensor,
-    ni: Tensor,
+@jax.jit
+def _calculate_neighbor_mask_per_atom(
+    aid: Tensor,
+    rij: Tensor,
     r_cutoff: Tensor,
-) -> Tuple[Tensor, Tensor]:
+) -> Tensor:
+    # Select atoms inside cutoff radius
+    mask = rij < r_cutoff
+    # exclude self-counting
+    mask = mask.at[aid].set(False)
+    return mask
+
+
+_vmap_calculate_neighbor_mask = jax.vmap(
+    _calculate_neighbor_mask_per_atom,
+    in_axes=(0, 0, None),
+)
+
+
+@partial(jax.jit, static_argnums=(0,))  # FIXME
+def _calculate_neighbor_mask(
+    structure,
+    r_cutoff: Tensor,
+) -> Tensor:
     # Tensors no need to be differentiable here
-    # TODO: optimization vmap
-    for aid in range(structure.natoms):
-        # TODO call jit kernel of distance calculation
-        rij, _ = structure.calculate_distance(aid)
-        # Get atom indices within the cutoff radius
-        ni_ = jnp.nonzero(rij < r_cutoff)[0]
-        # avoid self-counting atom index
-        ni_ = ni_[ni_ != aid]
-        # Set neighbor list tensors
-        nn = nn.at[aid].set(ni_.shape[0])
-        ni = ni.at[aid, : nn[aid]].set(ni_)
-    return nn, ni
+    aids = jnp.arange(structure.natoms)  # all atoms
+    rij, _ = structure.calculate_distance(aids)
+    return _vmap_calculate_neighbor_mask(aids, rij, r_cutoff)
 
 
 class Neighbor(_Base):
@@ -65,43 +72,41 @@ class Neighbor(_Base):
         self.r_cutoff = r_cutoff
         self.r_cutoff_updated = True
 
-    def _init_tensors(self, structure) -> None:
-        """
-        Allocating tensors for the neighbor list from the input structure.
+    # def _init_tensors(self, structure) -> None:
+    #     """
+    #     Allocating tensors for the neighbor list from the input structure.
 
-        :param structure: An instance of Structure
-        :type structure: Dict[Tensor]
-        """
-        # FIXME: reduce natoms*natoms tensor size!
-        # TODO: define max_num_neighbor to avoid extra memory allocation!
+    #     :param structure: An instance of Structure
+    #     :type structure: Dict[Tensor]
+    #     """
+    #     # FIXME: reduce natoms*natoms tensor size!
+    #     # TODO: define max_num_neighbor to avoid extra memory allocation!
 
-        # Avoid re-allocating structure with the same size
-        try:
-            if structure.natoms == len(self.number):
-                logger.debug("Avoid re-allocating the neighbor tensors")
-                return
-        except AttributeError:
-            pass
+    #     # Avoid re-allocating structure with the same size
+    #     try:
+    #         if structure.natoms == len(self.number):
+    #             logger.debug("Avoid re-allocating the neighbor tensors")
+    #             return
+    #     except AttributeError:
+    #         pass
 
-        logger.debug("Allocating tensors for neighbor list")
-        self.tensors = {
-            "number": jnp.empty(
-                structure.natoms,
-                dtype=int,  # dtype.UINT, FIXME
-                # device=structure.device,
-            ),
-            "index": jnp.empty(
-                (structure.natoms, structure.natoms),
-                dtype=int,  # dtype.INDEX, FIXME
-                # device=structure.device,
-            ),
-        }
+    #     logger.debug("Allocating tensors for neighbor list")
+    #     self.tensors = {
+    #         "number": jnp.empty(
+    #             structure.natoms,
+    #             dtype=int,  # dtype.UINT, FIXME
+    #         ),
+    #         "index": jnp.empty(
+    #             (structure.natoms, structure.natoms),
+    #             dtype=int,  # dtype.INDEX, FIXME
+    #         ),
+    #     }
 
-        for attr, tensor in self.tensors.items():
-            logger.debug(
-                f"{attr:12} -> Tensor(shape='{tensor.shape}', dtype='{tensor.dtype}'"
-            )
-        set_as_attribute(self, self.tensors)
+    #     for attr, tensor in self.tensors.items():
+    #         logger.debug(
+    #             f"{attr:12} -> Tensor(shape='{tensor.shape}', dtype='{tensor.dtype}'"
+    #         )
+    #     set_as_attribute(self, self.tensors)
 
     def update(self, structure) -> None:
         """
@@ -117,16 +122,14 @@ class Neighbor(_Base):
             logger.debug("Skipped updating the neighbor list")
             return
 
-        self._init_tensors(structure)
+        # self._init_tensors(structure) # FIXME: remove
 
         # ----------------------------------------
         logger.debug("Updating neighbor list")
 
-        self.number, self.index = _update(
+        self.mask = _calculate_neighbor_mask(
             structure,
-            self.number,
-            self.index,
-            self.r_cutoff,
+            jnp.atleast_1d(self.r_cutoff),
         )
 
         # Avoid updating the neighbor list the next time
