c49acaa612b7a86aa91c9f96c4d72bac30b895f2
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-11-19 18:38:24 +0100

    redfined nn model using flax

--- mlpot/models/nn.py
+++ mlpot/models/nn.py
@@ -1,96 +1,86 @@
 from ..logger import logger
 from .base import BaseModel
-from typing import Tuple, List
+from typing import Tuple, List, Dict, Callable
 from pathlib import Path
-from torch import nn, Tensor
-import torch
+from frozendict import frozendict
+from flax import linen as nn
+import jax.numpy as jnp
 
 
-class NeuralNetworkModel(BaseModel):
+Tensor = jnp.ndarray
+
+
+class NeuralNetworkModel(nn.Module):  # BaseModel
     """
     A neural network model which maps descriptor values to energy and force (using gradient).
     """
 
-    def __init__(
-        self,
-        input_size: int,
-        hidden_layers: Tuple[Tuple[int, str]],
-        output_layer: Tuple[int, str] = (1, "l"),
-        weights_range: Tuple[int, int] = None,
-    ) -> None:
-        super().__init__()  # FIXME: logging not working because of multiple inheritance
+    # input_size: int
+    hidden_layers: Tuple[Tuple[int, str]]
+    output_layer: Tuple[int, str] = (1, "l")
+    weights_range: Tuple[int, int] = None
+    param_dtype: jnp.dtype = jnp.float32  # FIXME
 
-        self.input_size = input_size
-        self.hidden_layers = hidden_layers
-        self.output_layer = output_layer
-        self.weights_range = weights_range
-        self.network: nn.Module = self._create_network()
+    # see here https://compphysvienna.github.io/n2p2/api/neural_network.html?highlight=activation%20function
+    _activation_function_map: Dict[str, Callable] = frozendict(
+        {
+            "t": nn.tanh,
+            "l": lambda x: x,
+        }
+    )
 
-    def _create_layer(self, in_size: int, out_size: int) -> nn.Linear:
+    def setup(self) -> None:
+        """
+        Initialize stack of Dense layers and activation functions.
+        """
+        self.layers = self._create_network()
+
+    def _create_layer(self, features: int) -> nn.Dense:
         """
         Create a neural network layer and initialize weights and bias.
         See https://aiqm.github.io/torchani/examples/nnp_training.html#training-example
         """
-        layer = nn.Linear(in_size, out_size)
-        if self.weights_range is not None:
-            nn.init.uniform_(
-                layer.weight.data, self.weights_range[0], self.weights_range[1]
-            )
-        layer.bias.data.zero_()  # TODO: add bias as input argument
-        return layer
+        # FIXME: init weights using self.weights_range
+        # TODO: add bias as input argument
+        return nn.Dense(features, param_dtype=self.param_dtype)
 
-    def _create_network(self) -> nn.Sequential:
+    def __call__(self, inputs: Tensor) -> Tensor:
+        x = inputs
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def _create_network(self) -> List:
         """
         Create a network using provided parameters.
         """
         # TODO: add logging
-        # Prepare stack of layers
-        layers: List[nn.Module] = list()
-        in_size = self.input_size
+        layers: List = list()
         # Hidden layers
         for out_size, af_type in self.hidden_layers:
-            layers.append(self._create_layer(in_size, out_size))
-            layers.append(self._get_activation_function(af_type))
-            in_size = out_size
+            layers.append(self._create_layer(out_size))
+            layers.append(self._activation_function_map[af_type])
         # Output layer
-        layers.append(self._create_layer(in_size, self.output_layer[0]))
-        layers.append(self._get_activation_function(self.output_layer[1]))
+        layers.append(self._create_layer(self.output_layer[0]))
+        layers.append(self._activation_function_map[self.output_layer[1]])
 
-        # Build a sequential model
-        return nn.Sequential(*layers)
+        return layers
 
-    def _get_activation_function(self, activation_type: str) -> nn.Module:
-        """
-        Return the corresponding activation function.
-        See here https://compphysvienna.github.io/n2p2/api/neural_network.html?highlight=activation%20function
-        """
-        # TODO: use dict map instead
-        if activation_type == "t":
-            return nn.Tanh()
-        elif activation_type == "l":
-            return nn.Identity()  # No activation function
-        else:
-            logger.error(
-                f"Unknown activation function type '{activation_type}'",
-                exception=ValueError,
-            )
+    # def save(self, filename: Path) -> None:
+    #     """
+    #     Save model weights.
+    #     """
+    #     torch.save(self.state_dict(), str(filename))
 
-    def forward(self, x: Tensor) -> Tensor:
-        return self.network(x)
+    # def load(self, filename: Path) -> None:
+    #     """
+    #     Load model weights.
+    #     """
+    #     self.load_state_dict(torch.load(str(filename)))
+    #     self.eval()
 
-    def save(self, filename: Path) -> None:
-        """
-        Save model weights.
-        """
-        torch.save(self.state_dict(), str(filename))
-
-    def load(self, filename: Path) -> None:
-        """
-        Load model weights.
-        """
-        self.load_state_dict(torch.load(str(filename)))
-        self.eval()
-
-    # def __repr__(self) -> str:
-    #   return f"{self.__class__.__name__}(input_size={self.input_size}" \
-    #          f", hidden_layers={self.hidden_layers}, output_layer={self.output_layer})"
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}(input_size={self.input_size}"
+            f", hidden_layers={self.hidden_layers}, output_layer={self.output_layer})"
+        )
