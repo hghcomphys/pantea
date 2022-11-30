5faa9ddbf7388ea7ef297be79c9910677c734c22
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-11-30 23:09:56 +0100

    removed aid input

--- mlpot/structure/_neighbor.py
+++ mlpot/structure/_neighbor.py
@@ -5,20 +5,16 @@ from functools import partial
 
 @jax.jit
 def _calculate_cutoff_mask_per_atom(
-    aid: jnp.ndarray,
     rij: jnp.ndarray,
     r_cutoff: jnp.ndarray,
 ) -> jnp.ndarray:
-    # Select atoms inside cutoff radius
-    mask = rij <= r_cutoff
-    # exclude self-counting
-    mask = mask.at[aid].set(False)
-    return mask
+    # mask atoms only inside the cutoff radius and excluding self-counting
+    return (rij <= r_cutoff) & (rij != 0.0)  # FIXME: use jnp.isclose
 
 
 _vmap_calculate_neighbor_mask = jax.vmap(
     _calculate_cutoff_mask_per_atom,
-    in_axes=(0, 0, None),
+    in_axes=(0, None),
 )
 
 
@@ -30,4 +26,4 @@ def _calculate_cutoff_mask(
     # Tensors no need to be differentiable here
     aids = jnp.arange(structure.n_atoms)  # all atoms
     rij, _ = structure.calculate_distance(aids)
-    return _vmap_calculate_neighbor_mask(aids, rij, r_cutoff)
+    return _vmap_calculate_neighbor_mask(rij, r_cutoff)
