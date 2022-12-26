ea05102047321a0357b0673d40485d30dcfefdd0
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2022-12-26 13:27:58 +0100

    improved tokenize function

--- mlpot/utils/tokenize.py
+++ mlpot/utils/tokenize.py
@@ -2,23 +2,21 @@ from typing import List, Optional, Tuple
 
 
 def tokenize(
-    line: str, comment: Optional[str] = None
-) -> Tuple[Optional[str], Optional[List[str]]]:
-    """
-    An utility method to read the input line as a keyword and list of tokens.
-    """
+    line: str,
+    comment: Optional[str] = None,
+) -> Tuple[Optional[str], List[str]]:
+    """An utility function to read the input line as a keyword and list of tokens."""
     # Skip comments
     if comment is not None:
         if line.startswith(comment):
-            return (None, None)
+            return (None, list())
         else:
             line = line[: line.find(comment)]
-
     # Find keyword and values
     tokens: List[str] = line.rstrip("/n").split()
     if len(tokens) > 1:
         return (tokens[0].lower(), tokens[1:])
     elif len(tokens) > 0:
-        return (tokens[0].lower(), None)
+        return (tokens[0].lower(), list())
     else:
-        return (None, None)
+        return (None, list())
