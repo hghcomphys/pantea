f46af837578afd2acd4d887ba75cee2e9d5de529
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2023-03-10 09:39:37 +0100

    update to v0.5.2 due to improvements on gradient descent updater

--- jaxip/_version.py
+++ jaxip/_version.py
@@ -5,6 +5,6 @@ def _version_as_tuple(version_str):
     return tuple(int(i) for i in version_str.split(".") if i.isdigit())
 
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 __version_info__ = _version_as_tuple(__version__)
