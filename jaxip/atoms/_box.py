613812f3377d6795feab068c06aa6178c4806f4e
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2023-05-01 08:38:34 +0200

    fixed a bug

--- jaxip/atoms/_box.py
+++ jaxip/atoms/_box.py
@@ -8,7 +8,7 @@ from jaxip.types import Array
 def _apply_pbc(dx: Array, lattice: Array) -> Array:
     """Apply periodic boundary condition (PBC) along x,y, and z directions."""
     box = lattice.diagonal()
-    dx = jnp.where(dx > 0.5e0 * box, dx - box, box)
+    dx = jnp.where(dx > 0.5e0 * box, dx - box, dx)
     dx = jnp.where(dx < -0.5e0 * box, dx + box, dx)
 
     return dx
