1932c7f5e6b1109986849612f1023a4ef024957f
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2023-07-16 15:38:44 +0200

    refactored ACSF cutoff function

--- jaxip/descriptors/acsf/cutoff.py
+++ jaxip/descriptors/acsf/cutoff.py
@@ -16,6 +16,22 @@ _TANH_PRE: float = ((math.e + 1 / math.e) / (math.e - 1 / math.e)) ** 3
 class CutoffFunction(BaseJaxPytreeDataClass):
     """Cutoff function for ACSF descriptor.
 
+    Cutoff functions are utilized in the calculation of Atom-centered Symmetry
+    Function (ACSF) descriptors. These functions serve to limit the influence
+    of atoms located beyond a specified distance from the central atom.
+
+    The ACSF descriptors employ cutoff functions to determine the range within which
+    neighboring atoms contribute to the descriptor calculation.
+    The cutoff function assigns a weight to each neighbor atom based on its
+    distance from the central atom. Typically, a smooth cutoff function is
+    employed to smoothly taper off the contribution of
+    atoms as they move away from the central atom.
+
+    The choice of cutoff function can vary depending on the specific application.
+    Examples of commonly used cutoff functions include the hyperbolic tangent (tanh)
+    cutoff, exponential, or exponential.
+
+
     See `cutoff function`_ and `cutoff type`_ for more details.
 
     .. _`cutoff function`: https://compphysvienna.github.io/n2p2/api/cutoff_functions.html?highlight=cutoff#
@@ -27,23 +43,22 @@ class CutoffFunction(BaseJaxPytreeDataClass):
     cutoff_function: Optional[Callable] = None
 
     def __post_init__(self) -> None:
-        self.cutoff_type = self.cutoff_type.lower()
         if self.cutoff_function is None:
             try:
-                self.cutoff_function = getattr(self, f"{self.cutoff_type}")
+                self.cutoff_function = getattr(
+                    self, f"{self.cutoff_type.lower()}"
+                )
             except AttributeError:
                 logger.error(
-                    f"'{self.__class__.__name__}' has no cutoff function '{self.cutoff_type}'",
+                    f"Unknown cutoff function '{self.cutoff_type}'",
                     exception=NotImplementedError,
                 )
+        else:
+            self.cutoff_type = self.cutoff_function.__name__
         self._assert_jit_dynamic_attributes()
         self._assert_jit_static_attributes(
             expected=("r_cutoff", "cutoff_type", "cutoff_function")
         )
-
-    def __hash__(self) -> int:
-        """Enforce to use the parent class's hash method (JIT)."""
-        return super().__hash__()
 
     @jax.jit
     def __call__(self, r: Array) -> Array:
@@ -74,8 +89,16 @@ class CutoffFunction(BaseJaxPytreeDataClass):
     def poly2(self, r: Array) -> Array:
         return ((15.0 - 6.0 * r) * r - 10) * r**3 + 1.0
 
+    def __hash__(self) -> int:
+        """Enforce to use the parent class's hash method (JIT)."""
+        return super().__hash__()
+
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(r_cutoff={self.r_cutoff}, cutoff_type='{self.cutoff_type}')"
+        return (
+            f"{self.__class__.__name__}"
+            f"(r_cutoff={self.r_cutoff}"
+            f", cutoff_type='{self.cutoff_type}')"
+        )
 
 
 register_jax_pytree_node(CutoffFunction)
