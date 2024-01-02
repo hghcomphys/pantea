1bb08110d8c6492e4895f1fc96b1f836e1955996
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2024-01-02 09:40:03 +0100

    fixed a potential bug in calculating distances

--- pantea/atoms/distance.py
+++ pantea/atoms/distance.py
@@ -9,21 +9,21 @@ from pantea.types import Array
 
 @jax.jit
 def _calculate_distances_per_atom(
-    atom_positions: Array,
+    atom_position: Array,
     neighbor_positions: Array,
     lattice: Optional[Array] = None,
 ) -> Tuple[Array, Array]:
     """Calculate distances between a single atom and neighboring atoms."""
-    dx: Array = atom_positions - neighbor_positions
+    dx = atom_position - neighbor_positions
     if lattice is not None:
         dx = _apply_pbc(dx, lattice)
-    # Fix NaN in gradient of np.linalg.norm
+    # Fix NaN in gradient of np.linalg.norm for zero distances
     # see https://github.com/google/jax/issues/3058
-    is_zero = dx.sum(axis=1, keepdims=True) == 0.0
-    dx_ = jnp.where(is_zero, jnp.ones_like(dx), dx)
-    dist = jnp.linalg.norm(dx_, ord=2, axis=1)  # type: ignore
-    dist = jnp.where(jnp.squeeze(is_zero), 0.0, dist)
-    return dist, dx
+    is_zero = jnp.prod(dx == 0.0, axis=1, keepdims=True, dtype=bool)
+    dx_masked = jnp.where(is_zero, 1.0, dx)
+    distances = jnp.linalg.norm(dx_masked, ord=2, axis=1)  # type: ignore
+    distances = jnp.where(is_zero[..., 1], 0.0, distances)
+    return distances, dx
 
 
 _vmap_calculate_distances: Callable = jax.vmap(
@@ -40,4 +40,8 @@ def _calculate_distances(
 ) -> Tuple[Array, Array]:
     """Calculate an array of distances between multiple atoms
     and the neighbors (using `jax.vmap`)."""
-    return _vmap_calculate_distances(atom_positions, neighbor_positions, lattice)
+    return _vmap_calculate_distances(
+        atom_positions,
+        neighbor_positions,
+        lattice,
+    )
