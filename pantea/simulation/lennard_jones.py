c5a9a30570ed4d7b6a7f1752650d01bf8f84fc11
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2024-01-07 14:33:18 +0100

    jit optimized lj potential

--- pantea/simulation/lennard_jones.py
+++ pantea/simulation/lennard_jones.py
@@ -1,6 +1,5 @@
 import os
-from functools import partial
-from typing import Protocol
+from typing import NamedTuple
 
 import jax
 import jax.numpy as jnp
@@ -13,19 +12,33 @@ os.environ["JAX_ENABLE_X64"] = "1"
 os.environ["JAX_PLATFORM_NAME"] = "cpu"
 
 
-class LJPotentialParams(Protocol):
-    epsilon: float
-    sigma: float
+class LJPotentialParams(NamedTuple):
+    epsilon: Array
+    sigma: Array
 
 
-@partial(jax.jit, static_argnums=0)
+# @jax.jit
 def _compute_pair_energies(params: LJPotentialParams, r: Array) -> Array:
     term = params.sigma / r
     term6 = term**6
     return 4.0 * params.epsilon * term6 * (term6 - 1.0)
 
 
-@partial(jax.jit, static_argnums=0)
+@jax.jit
+def _compute_total_energy(
+    params: LJPotentialParams,
+    positions: Array,
+    lattice: Array,
+    r_cutoff: Array,
+) -> Array:
+    masks, (rij, _) = _calculate_masks_with_aux_from_structure(
+        positions, r_cutoff, lattice
+    )
+    pair_energies = _compute_pair_energies(params, rij)
+    return 0.5 * jnp.sum(jnp.where(masks, pair_energies, 0.0))  # type: ignore
+
+
+# @jax.jit
 def _compute_pair_forces(params: LJPotentialParams, r: Array, R: Array) -> Array:
     term = params.sigma / r
     term6 = term**6
@@ -33,37 +46,51 @@ def _compute_pair_forces(params: LJPotentialParams, r: Array, R: Array) -> Array
     return jnp.expand_dims(force_factor, axis=-1) * R
 
 
+@jax.jit
+def _compute_forces(
+    params: LJPotentialParams,
+    positions: Array,
+    lattice: Array,
+    r_cutoff: Array,
+) -> Array:
+    masks, (rij, Rij) = _calculate_masks_with_aux_from_structure(
+        positions, r_cutoff, lattice
+    )
+    pair_forces = jnp.where(
+        jnp.expand_dims(masks, axis=-1),
+        _compute_pair_forces(params, rij, Rij),
+        jnp.zeros_like(Rij),
+    )
+    return jnp.sum(pair_forces, axis=1)  # type: ignore
+
+
 class LJPotential:
-    """An implementation of Lennard-Jones potential."""
+    """An implementation of Lennard-Jones potential for a single type atom."""
 
     def __init__(
         self,
-        sigma: float,
-        epsilon: float,
-        r_cutoff: float,
+        sigma: Array,
+        epsilon: Array,
+        r_cutoff: Array,
     ) -> None:
-        self.sigma = sigma
-        self.epsilon = epsilon
-        self.r_cutoff = r_cutoff
+        self.sigma = jnp.array(sigma)
+        self.epsilon = jnp.array(epsilon)
+        self.r_cutoff = jnp.array(r_cutoff)
 
-    # @partial(jax.jit, static_argnums=0)
     def __call__(self, structure: Structure) -> Array:
-        """Compute total energy."""
-        masks, (rij, _) = _calculate_masks_with_aux_from_structure(
-            structure.positions, self.r_cutoff, structure.lattice
+        """Compute total potential energy."""
+        return _compute_total_energy(
+            LJPotentialParams(self.epsilon, self.sigma),
+            structure.positions,
+            structure.lattice,
+            self.r_cutoff,
         )
-        pair_energies = _compute_pair_energies(self, rij)
-        return 0.5 * jnp.where(masks, pair_energies, 0.0).sum()  # type: ignore
 
-    # @partial(jax.jit, static_argnums=0)
     def compute_forces(self, structure: Structure) -> Array:
         """Compute force component for each atoms."""
-        masks, (rij, Rij) = _calculate_masks_with_aux_from_structure(
-            structure.positions, self.r_cutoff, structure.lattice
+        return _compute_forces(
+            LJPotentialParams(self.epsilon, self.sigma),
+            structure.positions,
+            structure.lattice,
+            self.r_cutoff,
         )
-        pair_forces = jnp.where(
-            jnp.expand_dims(masks, axis=-1),
-            _compute_pair_forces(self, rij, Rij),
-            jnp.zeros_like(Rij),
-        )
-        return jnp.sum(pair_forces, axis=1)  # type: ignore
