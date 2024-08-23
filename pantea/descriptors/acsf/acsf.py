12e6d2f82ba4e450aa340dbe5b5f8fe73991a8a6
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2024-08-23 10:57:24 +0200

    improved acsf calculation and namings

--- pantea/descriptors/acsf/acsf.py
+++ pantea/descriptors/acsf/acsf.py
@@ -1,17 +1,16 @@
 import itertools
 from dataclasses import dataclass
 from functools import partial
-from typing import Callable, Dict, Optional, Protocol, Tuple
+from typing import Callable, Dict, Optional, Tuple
 
-import jax
 import jax.numpy as jnp
-from jax import lax
+from jax import jacfwd, jit, lax, vmap
 
 from pantea.atoms.distance import (
     _calculate_distances_per_atom,
     _calculate_distances_with_aux_per_atom,
 )
-from pantea.atoms.neighbor import _calculate_masks_per_atom
+from pantea.atoms.neighbor import _calculate_cutoff_masks_per_atom
 from pantea.atoms.structure import Structure
 from pantea.descriptors.acsf.angular import AngularSymmetryFunction
 from pantea.descriptors.acsf.radial import RadialSymmetryFunction
@@ -20,191 +19,8 @@ from pantea.logger import logger
 from pantea.pytree import BaseJaxPytreeDataClass, register_jax_pytree_node
 from pantea.types import Array, Element
 
-
-def _calculate_radial_acsf_per_atom(
-    radial: Dict[NeighborElements, RadialSymmetryFunction],
-    atype: Array,
-    dist_i: Array,
-    emap: Dict[Element, Array],
-) -> Array:
-    elements = [k for k in radial.keys()][0]
-
-    mask_cutoff_i = _calculate_masks_per_atom(
-        dist_i, jnp.array(radial[elements].r_cutoff)
-    )
-    mask_cutoff_and_atype_ij = mask_cutoff_i & (atype == emap[elements.neighbor_j])
-
-    return jnp.sum(
-        radial[elements](dist_i),
-        where=mask_cutoff_and_atype_ij,
-        axis=0,
-    )
-
-
-def _calculate_angular_acsf_per_atom(
-    angular: Dict[NeighborElements, AngularSymmetryFunction],
-    atype: Array,
-    diff_i: Array,
-    dist_i: Array,
-    lattice: Array,
-    emap: Dict[Element, Array],
-) -> Array:
-    elements = [k for k in angular.keys()][0]
-
-    # cutoff-radius mask
-    mask_cutoff_i = _calculate_masks_per_atom(
-        dist_i, jnp.array(angular[elements].r_cutoff)
-    )
-    # mask for neighboring element j
-    at_j = emap[elements.neighbor_j]
-    mask_cutoff_and_atype_ij = mask_cutoff_i & (atype == at_j)
-    # mask for neighboring element k
-    at_k = emap[elements.neighbor_k]  # type: ignore
-    mask_cutoff_and_atype_ik = mask_cutoff_i & (atype == at_k)
-
-    total, _ = lax.scan(
-        partial(
-            _inner_loop_over_angular_acsf_terms,
-            mask_ik=mask_cutoff_and_atype_ik,
-            diff_i=diff_i,
-            dist_i=dist_i,
-            lattice=lattice,
-            kernel=angular[elements],
-        ),
-        jnp.array(0.0),
-        (diff_i, dist_i, mask_cutoff_and_atype_ij),
-    )
-
-    # correct double-counting
-    total = lax.cond(
-        at_j == at_k,
-        lambda x: x * 0.5,
-        lambda x: x,
-        total,
-    )
-    return total
-
-
-# Called by lax.scan (no need for @jax.jit)
-def _inner_loop_over_angular_acsf_terms(
-    total: Array,
-    inputs: Tuple[Array, ...],
-    diff_i: Array,
-    dist_i: Array,
-    mask_ik: Array,
-    lattice: Array,
-    kernel: Callable,
-) -> Tuple[Array, Array]:
-    # Scan occurs along the leading axis
-    Rij, rij, mask_ij = inputs
-
-    # rik = jnp.where(
-    #     mask_cutoff_and_atype_ik,
-    #     dist_i,
-    #     0.0,
-    # ) # same for Rjk if using diff_i
-
-    # fix nan issue in gradient
-    # see https://github.com/google/jax/issues/1052#issuecomment-514083352
-    operand = rij * dist_i
-    is_zero = operand == 0.0
-    true_op = jnp.where(is_zero, 1.0, operand)
-    cost = jnp.where(
-        mask_ik,
-        jnp.inner(Rij, diff_i) / true_op,
-        1.0,
-    )
-    cost = jnp.where(is_zero, 0.0, cost)  # type: ignore
-
-    rjk = jnp.where(  # diff_jk = diff_ji - diff_ik
-        mask_ik,
-        _calculate_distances_per_atom(Rij, diff_i, lattice),
-        0.0,
-    )  # second tuple output for Rjk
-
-    value = jnp.where(
-        mask_ij,
-        jnp.sum(
-            kernel(rij, dist_i, rjk, cost),
-            where=mask_ik & (rjk > 0.0),  # exclude k=j # type:ignore
-            axis=0,
-        ),
-        0.0,
-    )
-
-    return total + value, value
-
-
-class AcsfInterface(Protocol):
-    num_symmetry_functions: int
-    num_radial_symmetry_functions: int
-    num_angular_symmetry_functions: int
-    radial_symmetry_functions: Tuple
-    angular_symmetry_functions: Tuple
-
-
-def _calculate_descriptor_per_atom(
-    acsf: AcsfInterface,
-    single_atom_position: Array,
-    neighbor_positions: Array,
-    atype: Array,
-    lattice: Array,
-    emap: Dict,
-) -> Array:
-    """
-    Compute descriptor values per atom in the structure (via atom id).
-    """
-    dtype = single_atom_position.dtype
-    result: Array = jnp.empty(acsf.num_symmetry_functions, dtype=dtype)
-
-    dist_i, diff_i = _calculate_distances_with_aux_per_atom(
-        single_atom_position, neighbor_positions, lattice
-    )
-
-    # Loop over the radial terms
-    for index, (radial, elements) in enumerate(acsf.radial_symmetry_functions):
-        result = result.at[index].set(
-            _calculate_radial_acsf_per_atom({elements: radial}, atype, dist_i, emap)
-        )
-
-    # Loop over the angular terms
-    for index, (angular, elements) in enumerate(
-        acsf.angular_symmetry_functions,
-        start=acsf.num_radial_symmetry_functions,
-    ):
-        result = result.at[index].set(
-            _calculate_angular_acsf_per_atom(
-                {elements: angular}, atype, diff_i, dist_i, lattice, emap
-            )
-        )
-
-    return result
-
-
-_calculate_descriptor = jax.vmap(
-    _calculate_descriptor_per_atom,
-    in_axes=(None, 0, None, None, None, None),
-)
-
-_jitted_calculate_descriptor = jax.jit(
-    _calculate_descriptor,
-    static_argnums=(0,),
-)
-
-_calculate_grad_descriptor_per_atom = jax.jacfwd(
-    _calculate_descriptor_per_atom,
-    argnums=1,
-)
-
-_calculate_grad_descriptor = jax.vmap(
-    _calculate_grad_descriptor_per_atom,
-    in_axes=(None, 0, None, None, None, None),
-)
-
-_jitted_calculate_grad_descriptor = jax.jit(
-    _calculate_grad_descriptor,
-    static_argnums=(0,),
-)
+AssignedRadialSymmetryFunction = Tuple[RadialSymmetryFunction, NeighborElements]
+AssignedAngularSymmetryFunction = Tuple[AngularSymmetryFunction, NeighborElements]
 
 
 @dataclass
@@ -225,12 +41,8 @@ class ACSF(BaseJaxPytreeDataClass):
     """
 
     central_element: str
-    radial_symmetry_functions: Tuple[
-        Tuple[RadialSymmetryFunction, NeighborElements], ...
-    ]
-    angular_symmetry_functions: Tuple[
-        Tuple[AngularSymmetryFunction, NeighborElements], ...
-    ]
+    radial_symmetry_functions: Tuple[AssignedRadialSymmetryFunction, ...]
+    angular_symmetry_functions: Tuple[AssignedAngularSymmetryFunction, ...]
 
     def __call__(
         self,
@@ -267,7 +79,7 @@ class ACSF(BaseJaxPytreeDataClass):
                     exception=ValueError,
                 )
 
-        return _jitted_calculate_descriptor(
+        return _jitted_calculate_acsf_descriptor(
             self,
             structure.positions[index],
             structure.positions,
@@ -304,7 +116,7 @@ class ACSF(BaseJaxPytreeDataClass):
                 )
             positions = structure.positions[index]
 
-        return _jitted_calculate_grad_descriptor(
+        return _jitted_calculate_grad_acsf_descriptor(
             self,
             positions,
             structure.positions,
@@ -352,6 +164,186 @@ class ACSF(BaseJaxPytreeDataClass):
             f"{self.__class__.__name__}(central_element='{self.central_element}'"
             f", num_symmetry_functions={self.num_symmetry_functions})"
         )
+
+
+def _calculate_acsf_descriptor_per_atom(
+    acsf: ACSF,
+    atom_position: Array,
+    neighbor_atom_positions: Array,
+    atom_types: Array,
+    lattice: Array,
+    element_map: Dict[Element, Array],
+) -> Array:
+    """Compute the ACSF descriptor values per atom."""
+    result: Array = jnp.empty(acsf.num_symmetry_functions, dtype=atom_position.dtype)
+
+    distances_i, position_differences_i = _calculate_distances_with_aux_per_atom(
+        atom_position, neighbor_atom_positions, lattice
+    )
+    # Loop over the radial terms
+    for index, (symmetry_function, assigned_elements) in enumerate(
+        acsf.radial_symmetry_functions
+    ):
+        result = result.at[index].set(
+            _calculate_radial_acsf_per_atom(
+                symmetry_function,
+                distances_i,
+                atom_types,
+                element_map[assigned_elements.neighbor_j],
+            )
+        )
+    # Loop over the angular terms
+    for index, (symmetry_function, assigned_elements) in enumerate(
+        acsf.angular_symmetry_functions,
+        start=acsf.num_radial_symmetry_functions,
+    ):
+        result = result.at[index].set(
+            _calculate_angular_acsf_per_atom(
+                symmetry_function,
+                atom_types,
+                position_differences_i,
+                distances_i,
+                lattice,
+                element_map[assigned_elements.neighbor_j],
+                element_map[assigned_elements.neighbor_k],  # type: ignore
+            )
+        )
+    return result
+
+
+_calculate_acsf_descriptor = vmap(
+    _calculate_acsf_descriptor_per_atom,
+    in_axes=(None, 0, None, None, None, None),
+)
+
+_jitted_calculate_acsf_descriptor = jit(
+    _calculate_acsf_descriptor,
+    static_argnums=(0,),
+)
+
+_calculate_grad_acsf_descriptor_per_atom = jacfwd(
+    _calculate_acsf_descriptor_per_atom,
+    argnums=1,
+)
+
+_calculate_grad_acsf_descriptor = vmap(
+    _calculate_grad_acsf_descriptor_per_atom,
+    in_axes=(None, 0, None, None, None, None),
+)
+
+_jitted_calculate_grad_acsf_descriptor = jit(
+    _calculate_grad_acsf_descriptor,
+    static_argnums=(0,),
+)
+
+
+def _calculate_radial_acsf_per_atom(
+    radial_symmetry_function: RadialSymmetryFunction,
+    distances_i: Array,
+    atom_types: Array,
+    neighbor_atom_type_j: int,
+) -> Array:
+    r_cutoff = jnp.array(radial_symmetry_function.r_cutoff)
+    cutoff_mask_i = _calculate_cutoff_masks_per_atom(distances_i, r_cutoff)
+    cutoff_masks_and_atom_types_ij = cutoff_mask_i & (
+        atom_types == neighbor_atom_type_j
+    )
+    return jnp.sum(
+        radial_symmetry_function(distances_i),
+        where=cutoff_masks_and_atom_types_ij,
+        axis=0,
+    )
+
+
+def _calculate_angular_acsf_per_atom(
+    angular_symmetry_function: AngularSymmetryFunction,
+    atom_types: Array,
+    position_differences_i: Array,
+    distances_i: Array,
+    lattice: Array,
+    neighbor_atom_type_j: int,
+    neighbor_atom_type_k: int,
+) -> Array:
+
+    # cutoff-radius masks
+    r_cutoff = jnp.array(angular_symmetry_function.r_cutoff)
+    cutoff_masks_i = _calculate_cutoff_masks_per_atom(distances_i, r_cutoff)
+    # masks for neighboring element j
+    cutoff_masks_and_atom_types_ij = cutoff_masks_i & (
+        atom_types == neighbor_atom_type_j
+    )
+    # masks for neighboring element k
+    cutoff_masks_and_atom_types_ik = cutoff_masks_i & (
+        atom_types == neighbor_atom_type_k
+    )
+    # angular terms
+    total, _ = lax.scan(
+        partial(
+            _inner_loop_over_angular_acsf_terms,
+            mask_ik=cutoff_masks_and_atom_types_ik,
+            position_differences_i=position_differences_i,
+            distances_i=distances_i,
+            lattice=lattice,
+            kernel=angular_symmetry_function,
+        ),
+        jnp.array(0.0),
+        (position_differences_i, distances_i, cutoff_masks_and_atom_types_ij),
+    )
+    # correct the double-counting
+    return lax.cond(
+        neighbor_atom_type_j == neighbor_atom_type_k,
+        _divide_by_two,
+        _unary,
+        total,
+    )  # type: ignore
+
+
+# Called by lax.scan (no need for @jax.jit)
+def _inner_loop_over_angular_acsf_terms(
+    total: Array,
+    inputs: Tuple[Array, ...],
+    position_differences_i: Array,
+    distances_i: Array,
+    mask_ik: Array,
+    lattice: Array,
+    kernel: Callable[[Array, Array, Array, Array], Array],
+) -> Tuple[Array, Array]:
+    # Scan occurs along the leading axis
+    Rij, rij, mask_ij = inputs
+    # fix nan issue in gradient
+    # see https://github.com/google/jax/issues/1052#issuecomment-514083352
+    operand = rij * distances_i
+    is_zero = operand == 0.0
+    true_op = jnp.where(is_zero, 1.0, operand)
+    cost = jnp.where(
+        mask_ik,
+        jnp.inner(Rij, position_differences_i) / true_op,
+        1.0,
+    )
+    cost = jnp.where(is_zero, 0.0, cost)  # type: ignore
+    rjk = jnp.where(  # diff_jk = diff_ji - position_differences_ik
+        mask_ik,
+        _calculate_distances_per_atom(Rij, position_differences_i, lattice),
+        0.0,
+    )  # second tuple output for Rjk
+    value = jnp.where(
+        mask_ij,
+        jnp.sum(
+            kernel(rij, distances_i, rjk, cost),
+            where=mask_ik & (rjk > 0.0),  # exclude k=j # type:ignore
+            axis=0,
+        ),
+        0.0,
+    )
+    return total + value, value
+
+
+def _unary(array: Array) -> Array:
+    return array
+
+
+def _divide_by_two(array: Array) -> Array:
+    return array * 0.5
 
 
 register_jax_pytree_node(ACSF)
